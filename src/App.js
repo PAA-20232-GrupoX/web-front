9f5662d6088d5acc85bbd87eef864647c6198c0b
ciriatico <gabrielciriatico@gmail.com>
2023-12-03 14:22:17 -0300

    Branch com visualização de árvore em React

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// App.js
+import React from 'react';
+import AnimatedGraph from './AnimatedGraph'; // Replace with the correct path to your AnimatedGraph component
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Sua árvore</h1>
+      <AnimatedGraph />
+    </div>
+  );
+}
+
+export default App;
