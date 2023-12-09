0ec063f076682456fa454960c7be22781ae68481
maldisco <filipe.sf04@gmail.com>
2023-12-08 23:12:57 -0300

    add admin page

---
+++ src/app/admin/page.js
@@ -0,0 +1,10 @@
+"use client"
+import Header from "@/components/header";
+
+export default function Admin() {
+  return (
+    <main className="w-full h-full bg-bglight">
+      <Header title="PAA 4" />
+    </main>
+  );
+}
